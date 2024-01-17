f80affbea61b7c962281ca90b9325ee81d69b914
nivarnas <rpankratoff@gmail.com>
2024-01-17 03:42:56 +0300

    feat: add solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -417,8 +417,22 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let res = str;
+
+  for (let i = 0; i < iterations; i += 1) {
+    let firstPart = '';
+    let secondPart = '';
+    for (let j = 0; j < res.length; j += 1) {
+      if (j % 2 === 0) {
+        firstPart += res.charAt(j);
+      } else {
+        secondPart += res.charAt(j);
+      }
+    }
+    res = firstPart + secondPart;
+  }
+  return res;
 }
 
 /**
